efa13f3f7c61af9bb89259bc8a4dac7157b139d4
Sarthak Saxena <sarthak.legendary@gmail.com>
2021-01-31 18:11:31 +0530

    Added 24/01/2021

---
+++ Sarthak/370B.cpp
@@ -0,0 +1,49 @@
+// Date- 24/01/2021
+// Problem Link- https://codeforces.com/problemset/problem/370/B, rated 1300
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool checkSubset(std::vector<int> a, std::vector<int> b){
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    vector<int> cut(a.size() + b.size());
+    vector<int>::iterator cutEnd;
+
+    cutEnd = set_intersection(a.begin(), a.end(), b.begin(), b.end(), cut.begin());
+    cut.resize(cutEnd - cut.begin());
+
+    if(cut.size() == b.size()){return 1;}
+    else{return 0;}
+
+}
+
+
+int main(){
+
+    int n(0);
+    scanf("%d", &n);
+    vector<std::vector<int>> numbers;
+    int newNumber(0);
+
+    for(int k = 0; k < n; k++){
+        int currentLength(0); scanf("%d", &currentLength);
+        vector<int> temp;
+        while(currentLength--){scanf("%d", &newNumber); temp.push_back(newNumber);}
+        numbers.push_back(temp);
+    }
+
+    for(int first = 0; first < n; first++){
+        bool subsetFlag = 0;
+        for(int second = 0; second < n; second++){
+            if(first == second){continue;}
+            if(checkSubset(numbers[first], numbers[second])){subsetFlag = 1; break;}
+        }
+        if(subsetFlag){puts("NO");} else{puts("YES");}
+    }
+
+    return 0;
+}
